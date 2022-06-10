3f6a4c108dd304d1c1c62d0976a4d07ea30f3297
NadimMahmud17 <nadim.bd62@gmail.com>
2022-06-10 09:40:21 -0700

    nextpc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pagess/Home/Home';
+import Profile from './pagess/Profile/Profile';
 
 
 
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <div className="blur" style={{top: "-18%", right: '0'}}></div>
       <div className="blur" style={{top: "36%", left: '-8rem'}}></div>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <Profile></Profile>
     </div>
   );
 }
